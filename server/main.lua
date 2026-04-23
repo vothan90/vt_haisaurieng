228430d8db893499337a5c5b97d93cc5208c9ac3
vothan90 <112075018+vothan90@users.noreply.github.com>
2026-04-23 08:20:21 +0700

    Create server/main.lua file with server initialization

---
+++ server/main.lua
@@ -0,0 +1,11 @@
+-- Server Initialization Script
+
+-- This script sets up the server and initializes necessary components
+
+function initServer()
+    print("Server is initializing...")
+    -- Add initialization code here
+end
+
+-- Call the initialization function
+initServer()
